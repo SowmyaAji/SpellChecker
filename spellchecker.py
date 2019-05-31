e5d244a2ae1c12e322b31369497d752f63b92ec8
Sowmya <sowmya.aji@gmail.com>
2019-05-30 20:55:01 -0400

    Add comments to the code

--- spellchecker.py
+++ spellchecker.py
@@ -1,4 +1,6 @@
-# from spellchecker import SpellChecker
+
+
+# open words.txt file which has about a 1,000 commonly used words
 
 words = []
 with open("words.txt") as new_file:
@@ -8,23 +10,27 @@ with open("words.txt") as new_file:
 
 
 def spell_checker(words):
-    # spell = SpellChecker()
+    """look at input word and check if it is valid or not. If invalid, make suggestions of similar words"""
+
     suggest_words = []
+    # get input
     i_word = input("Word to check: ").lower()
     if i_word in words:
         print("Yes it is a valid word")
     else:
-        # print(spell.correction(i_word))
-        # print(spell.candidates(i_word))
+
         for word in words:
             if len(i_word) == len(word):
                 for i in range(len(i_word)):
                     if i_word[i] in word:
+                        # get all words that have the input alphabets in them
                         suggest_words.append(word)
+
         count = {}
         suggest_arr = []
         word_arr = []
         for word in suggest_words:
+            # get the count of each word that is suggested. For the correct word, the count will be the same as the length of the word,as each iteration would have added that word to suggest_words list
             count[word] = suggest_words.count(word)
 
         for key, value in count.items():
