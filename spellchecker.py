f3964a45152a3ecd6ba607e08b586bf271883928
Sowmya <sowmya.aji@gmail.com>
2019-05-31 20:26:26 -0400

    Add Spellchecker class and clean up the code

--- spellchecker.py
+++ spellchecker.py
@@ -1,7 +1,7 @@
 
 import re
 
-# open words.txt file which has about 1,000 commonly used words
+# open words.txt file which has about 1,000 commonly used English wo
 
 
 def get_words():
@@ -12,36 +12,47 @@ def get_words():
             words.append(word)
         return words
 
+
 # check for third consecutive duplicate letter in input and remove it as an autofix (English doesn't have any word with three consecutive duplicates)
-
-
 def erase_triple_dupes(word):
-    regex = re.compile(r"(.)\1\1+")
-    return regex.sub(r"\1\1", word)
+    return re.sub(r"(\w)\1\1+", r"\1\1", word)
 
 
-def compute_suggested_words(in_word, words):
-    suggested_words = {}
-    for word in words:
-        if len(in_word) == len(word):
-            suggested_words[word] = compute_count(in_word, word)
-    return suggested_words
+class SpellCheck:
+    """A class that looks into the dictionary and checks for word suggestions for the inputted word"""
 
+    def __init__(self, words, in_word):
+        self.input_word = in_word
+        self.words = words
+        self.suggested_words = {}
 
-def compute_count(in_word, word):
-    count = 0
-    for i in range(len(in_word)):
-        if in_word[i] in word:
-            count += 1
-    return count
+    # look at words of similar length to the input word
+    def __compute_suggested_words(self):
+        if len(self.suggested_words) != 0:
+            return
+        for word in self.words:
+            if len(self.input_word) == len(word):
+                self.suggested_words[word] = self.__compute_count(word)
 
+    # count how many alphabets of the input word is in each word in the dictionary
+    def __compute_count(self, word):
+        count = 0
+        for i in range(len(self.input_word)):
+            if self.input_word[i] in word:
+                count += 1
+        return count
 
-def find_likely_words(count, in_word):
-    return [key for key, value in count.items() if value == len(in_word)]
+    # find the most likely word that has been misspelled as the input word, using count and the length
+    def find_likely_words(self):
+        self.__compute_suggested_words()
+        return [key for key, value in self.suggested_words.items() if value == len(self.input_word)]
 
+    # find the nearest options to the most likely word
+    def find_optional_words(self):
+        self.__compute_suggested_words()
+        return [key for key, value in self.suggested_words.items() if value == len(self.input_word) - 1]
 
-def find_optional_words(count, in_word):
-    return [key for key, value in count.items() if value == len(in_word) - 1]
+# get input word
 
 
 def ask():
@@ -49,20 +60,20 @@ def ask():
     # clean the input of redundant triple dupes
     return erase_triple_dupes(i_word)
 
+# check the input word against the dictionary of words and if it is a valid word, say so. Else use all the above methods to check for the nearest word to it and display those
+
 
 def spell_checker(words):
     """look at input word and check if it is a valid English word as per our limited dictionary, or not. If invalid, make suggestions of similar words"""
-    # get input
     in_word = ask()
     print("The word you have asked to check is: " + in_word)
     if in_word in words:
         print("Yes it is a valid word")
-    # if the word does not match any in our dictionary, suggest alternatives
     else:
-        count = compute_suggested_words(in_word, words)
+        speller = SpellCheck(words, in_word)
         print("This maybe the word you are looking for: ",
-              find_likely_words(count, in_word))
-        print("Options: ",  find_optional_words(count, in_word))
+              speller.find_likely_words())
+        print("Options: ",  speller.find_optional_words())
 
 
 spell_checker(get_words())
